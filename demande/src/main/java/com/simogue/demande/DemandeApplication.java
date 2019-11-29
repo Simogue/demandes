55b67d541ae7fbfed7508dd5ffd167e8061968fe
Simogue <elguennouni.mohammed@gmail.com>
2019-11-29 14:21:37 +0100

    Add files via upload

---
+++ demande/src/main/java/com/simogue/demande/DemandeApplication.java
@@ -0,0 +1,13 @@
+package com.simogue.demande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandeApplication.class, args);
+	}
+
+}
